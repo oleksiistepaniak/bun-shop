7a6c7ae2c76556e74aea72ce5477e59fc51b4e45
alex <alexstepanyak@gmail.com>
2024-12-26 01:03:34 +0200

    bun server created

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 3000,
+    fetch(_request: Request): Response | Promise<Response> {
+        return new Response("BUN IS WORKING!")
+    }
+})
+
+console.log(`Server was successfully started on the port: ${server.port}!`)
